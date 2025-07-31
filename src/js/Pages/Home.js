662d60a00119842028865871da0d3b6e1de34821
Afrim Avmedoski <afrim.avmedoski@gmail.com>
2025-07-31 11:31:06 +0200

    added projects section

--- src/js/Pages/Home.js
+++ src/js/Pages/Home.js
@@ -3,18 +3,16 @@ import Header from "../Components/Header/Header";
 import Banner from "../Components/Banner/Banner";
 import Section from "../Components/Section/Section";
 import Social from "../Components/Header/Social/Social";
-import gh from "../../assets/github.svg";
-import tw from "../../assets/twitter.svg";
-import ln from "../../assets/linkedin.svg";
 
 import html from "../../assets/html.svg";
 import css from "../../assets/css.svg";
 import js from "../../assets/js.svg";
-import jsx from "../../assets/react.svg";
+import react from "../../assets/react.svg";
 import sass from "../../assets/sass.svg";
 import git from "../../assets/git.svg";
 import code from "../../assets/code.svg";
 import gith from "../../assets/gith.svg";
+import Projects from "../Components/Projects/Projects";
 
 const Home = () => {
 	return (
@@ -31,11 +29,11 @@ const Home = () => {
 					items={[
 						{url: "#", src: html, alt: "HTML"},
 						{url: "#", src: css, alt: "CSS"},
-						{url: "#", src: js, alt: "JavaScript"},
-						{url: "#", src: jsx, alt: "React"},
-						{url: "#", src: sass, alt: "Sass"},
+						{url: "#", src: js, alt: "JS"},
+						{url: "#", src: react, alt: "React"},
+						{url: "#", src: sass, alt: "Saas"},
 						{url: "#", src: git, alt: "Git"},
-						{url: "#", src: code, alt: "Code"},
+						{url: "#", src: code, alt: "VS Code"},
 						{url: "#", src: gith, alt: "GitHub"},
 					]}
 				/>
@@ -44,7 +42,9 @@ const Home = () => {
 				type="projects"
 				title="Projects"
 				subtitle="Things I've built so far"
-			/>
+			>
+				<Projects />
+			</Section>
 			<Header type="footer" />
 		</>
 	);
