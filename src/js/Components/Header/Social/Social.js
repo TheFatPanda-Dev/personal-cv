416a3eb337557f26cfa1bb51a82057ace51e7533
Afrim Avmedoski <afrim.avmedoski@gmail.com>
2025-07-31 10:25:45 +0200

    added section my tech stack

--- src/js/Components/Header/Social/Social.js
+++ src/js/Components/Header/Social/Social.js
@@ -1,23 +1,16 @@
 import React from "react";
-import git from "../../../../assets/github.svg";
-import ln from "../../../../assets/linkedin.svg";
-import tw from "../../../../assets/twitter.svg";
+
 import "./_social.scss";
 
 const Social = (props) => {
 	return (
 		<div className={props.type + "__social"}>
-			<a href="#" className="href">
-				<img src={git} alt="github" />
-			</a>
-			<a href="#" className="href">
-				<img src={ln} alt="linkedin" />
-			</a>
-			<a href="#" className="href">
-				<img src={tw} alt="twitter" />
-			</a>
+			{props.items.map((item, i) => (
+				<a key={i} href={item.url}>
+					<img src={item.src} alt={item.alt} />
+				</a>
+			))}
 		</div>
 	);
 };
-
 export default Social;
--- src/js/Pages/Home.js
+++ src/js/Pages/Home.js
@@ -1,12 +1,50 @@
 import React from "react";
 import Header from "../Components/Header/Header";
 import Banner from "../Components/Banner/Banner";
+import Section from "../Components/Section/Section";
+import Social from "../Components/Header/Social/Social";
+import gh from "../../assets/github.svg";
+import tw from "../../assets/twitter.svg";
+import ln from "../../assets/linkedin.svg";
+
+import html from "../../assets/html.svg";
+import css from "../../assets/css.svg";
+import js from "../../assets/js.svg";
+import jsx from "../../assets/react.svg";
+import sass from "../../assets/sass.svg";
+import git from "../../assets/git.svg";
+import code from "../../assets/code.svg";
+import gith from "../../assets/gith.svg";
 
 const Home = () => {
 	return (
 		<>
 			<Header type="header" />
 			<Banner />
+			<Section
+				type="tech"
+				title="My Tech Stack"
+				subtitle="Technologies I've been working with recently"
+			>
+				<Social
+					type={"tech"}
+					items={[
+						{url: "#", src: html, alt: "HTML"},
+						{url: "#", src: css, alt: "CSS"},
+						{url: "#", src: js, alt: "JavaScript"},
+						{url: "#", src: jsx, alt: "React"},
+						{url: "#", src: sass, alt: "Sass"},
+						{url: "#", src: git, alt: "Git"},
+						{url: "#", src: code, alt: "Code"},
+						{url: "#", src: gith, alt: "GitHub"},
+					]}
+				/>
+			</Section>
+			<Section
+				type="projects"
+				title="Projects"
+				subtitle="Things I've built so far"
+			/>
 			<Header type="footer" />
 		</>
 	);
